4f7c1e6bc418152ddd1ffb17eac4fca2b86f435c
oladimeji <dimejiola@msn.com>
2022-01-30 14:26:07 +0000

    adde new functions

--- instagram.py
+++ instagram.py
@@ -126,27 +126,27 @@ def get_user_media(username_list, N=5):
 def comment_on_media(media_list:list,comment:str):
     """Comment on media and like the comment & post"""
     for media_id in media_list:
-        try:
-            media_id = dict(media_id)
-            with open('comment_log.json','r') as fp:
-                if media_id.get('code') not in fp.read():
-                    cl.media_like(media_id.get('pk'))
-                    comment_id = dict(cl.media_comment(media_id.get('pk'), comment))
-                    like_comment = cl.comment_like(comment_id.get('pk'))
-                    with open('comment_log.json','a+') as fp:
-                        json.dump({"commented_by_username": cl.username,"commented_by_id": cl.user_id,
-                        "media_slug" : "www.instagram.com/p/"+media_id.get('code'),
-                        "media_id":media_id.get('pk'), "comment_id": comment_id.get('pk'),
-                        "media_owner_id": dict(media_id.get('user')).get('pk'),
-                        "media_owner_username": dict(media_id.get('user')).get('username'),
-                        "comment_date":comment_id.get('created_at_utc').strftime("%Y-%m-%d %H:%M")},fp)
-                        fp.write('\n')
-                    print('Comment written on post www.instagram.com/p/{} comment_id = {}'.format(media_id.get('code'),comment_id.get('pk') ))
-                else:
-                    print('There is a comment on this post already')
-        except:
-            print('Error trying for {} '.format("www.instagram.com/p/"+dict(media_id).get('code')))
-            continue
+        # try:
+        media_id = dict(media_id)
+        with open('comment_log.json','r') as fp: #todo: find a way to make this file relative
+            if media_id.get('code') not in fp.read():
+                cl.media_like(media_id.get('pk'))
+                comment_id = dict(cl.media_comment(media_id.get('pk'), comment))
+                like_comment = cl.comment_like(comment_id.get('pk'))
+                with open('comment_log.json','a+') as fp:
+                    json.dump({"commented_by_username": cl.username,"commented_by_id": cl.user_id,
+                    "media_slug" : "www.instagram.com/p/"+media_id.get('code'),
+                    "media_id":media_id.get('pk'), "comment_id": comment_id.get('pk'),
+                    "media_owner_id": dict(media_id.get('user')).get('pk'),
+                    "media_owner_username": dict(media_id.get('user')).get('username'),
+                    "comment_date":comment_id.get('created_at_utc').strftime("%Y-%m-%d %H:%M")},fp)
+                    fp.write('\n')
+                print('Comment written on post www.instagram.com/p/{} comment_id = {}'.format(media_id.get('code'),comment_id.get('pk') ))
+            else:
+                print('There is a comment on this post already')
+        # except:
+        #     print('Error trying for {} '.format("www.instagram.com/p/"+dict(media_id).get('code')))
+        #     continue
 
 def delete_comment(media_id, media_comment_id):
     """Delete comment on media"""
@@ -175,9 +175,13 @@ def delete_media(username,password,N=5):
             continue
     logout()
 
-def follower_user(user_id, follow ='Y'):
-    """Follow a users"""
-    return cl.user_follow(user_id) if follow == 'Y' else cl.user_unfollow(user_id)
+def un_follow_user(user_id, follow ='follow'):
+    """Follow or unfollow a user"""
+    return cl.user_follow(user_id) if follow == 'follow' else cl.user_unfollow(user_id)
+
+def user_network(user_id, flow='following', N=25):
+    """Return a list of followers or user following the said user."""
+    return cl.user_following(user_id, amount = N) if flow == 'following' else cl.user_followers(user_id,amount= N)
 
 def user_info_by_urs(username):
     return cl.user_info_by_username(username)
@@ -188,7 +192,7 @@ def follow_and_comment(username, password,tags, comment, num_media,follow = 'Y')
     medias = get_hashtag_medias_top(tags,num_media)
     for user in medias:
         if follow == 'Y':
-            follower_user(user.dict().get('user').get('pk'))
+            un_follow_user(user.dict().get('user').get('pk'))
             print('Followed user: '+ user.dict().get('user').get('username'))
         comment_on_media([user],  comment)
     logout()
